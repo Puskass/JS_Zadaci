5a673a945f18ac0e6cf7bcd0614aa36e397e1403
Puskass <cosovictarik711@gmail.com>
2023-01-05 22:01:04 +0100

    uradjen treci zadatak

--- app.js
+++ app.js
@@ -8,5 +8,20 @@ console.log(zbir)
 // 2. Zadatak
 function toggleColor() {
     const element = document.body;
-    element.classList.toggle("black-bg");
+    element.classList.toggle("black-bg")
 }
+
+// 3. Zadatak
+const objects = [
+  { name: "Lopta", color: "red" },
+  { name: "Stol", color: "blue" },
+  { name: "Daska", color: "blue" },
+  { name: "Pernica", color: "white" },
+]
+
+function filterBlueObjects(objects){
+    return objects.filter(object => object.color === "blue")
+}
+  
+  const blueObjects = filterBlueObjects(objects)
+  console.log(blueObjects)
