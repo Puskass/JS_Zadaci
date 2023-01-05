16e7d427703e63ff271cc783605ce30a3a873eb9
Puskass <cosovictarik711@gmail.com>
2023-01-05 21:46:39 +0100

    uradjen drugi zadatak

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// 1. Zadatak
 function addTwoNumbers(num1, num2){
     return num1 + num2
 }
 let zbir = addTwoNumbers(2, 4)
 console.log(zbir)
+
+// 2. Zadatak
+function toggleColor() {
+    const element = document.body;
+    element.classList.toggle("black-bg");
+}
